c810cc6fcdc5691bfd3580c71b19cf3ffd388d0d
zx563147474 <zx563147474@gmail.com>
2019-09-14 22:59:19 -0500

    mac

--- open web cam/open_web_cam.py
+++ open web cam/open_web_cam.py
@@ -4,8 +4,8 @@ capture = cv2.VideoCapture(0)
 FPS = capture.get(5)
 print(FPS)
 # 定义编码方式并创建VideoWriter对象
-fourcc = cv2.VideoWriter_fourcc(*'avc1')
-outfile = cv2.VideoWriter('output1.mp4', fourcc, FPS, (640, 480))
+fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+outfile = cv2.VideoWriter('output1.mp4', fourcc, 20, (640, 480))
 while(capture.isOpened()):
     ret, frame = capture.read()
     if ret:
